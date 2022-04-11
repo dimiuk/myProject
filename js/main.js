f06a51f155b343baae326c997abdccdb255d0414
dimiuk <dmitryfakanov@gmail.com>
2022-04-11 20:16:14 +0100

    some Javascript

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const textInput = document.querySelector("#textarea");
+const emailInput = document.querySelector("#email");
+const telephoneInput = document.querySelector("#telephone");
+const button = document.querySelector("#button");
+const feedbackForm = document.querySelector("#feedback-form");
+const form = document.querySelector("#question-form");
+
+form.addEventListener("submit", validateForm);
+
+function validateForm(event) {
+  event.preventDefault();
+
+  let text = textInput.value;
+  let email = emailInput.value;
+  let tel = telephoneInput.value;
+
+  if (text.length > 10) {
+    feedbackForm.innerText = "Your question submitted succesfully";
+  } else {
+    feedbackForm.innerText = "Your question must be more than 10 characters";
+  }
+}
